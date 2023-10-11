87e2b3571f4f3ed02d652220c1bb2fe5394c07a9
OvadillaAisyahRahma <83963385+OvadillaAisyahRahma@users.noreply.github.com>
2023-10-12 01:57:29 +0700

    Update uts.py

--- uts.py
+++ uts.py
@@ -1,178 +1,259 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from numpy import array
+from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_percentage_error
-from datetime import datetime
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from numpy import array
+from sklearn import tree
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score, f1_score
+from sklearn.tree import DecisionTreeClassifier
+from collections import OrderedDict
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.datasets import make_classification
+from sklearn.svm import SVC
+import altair as alt
+from sklearn.utils.validation import joblib
 
-st.title('Prediksi Harga Emas Menggunakan Metode Linear Regression')
-
-# Mendapatkan data harga emas dari URL
-df = pd.read_csv('https://raw.githubusercontent.com/DwiAqilahP/kolaborasipro/main/PTRO.JK.csv')
-df.isnull().sum()
-df['Open'] = df['Open'].fillna(value=df['Open'].median())
-df['High'] = df['High'].fillna(value=df['High'].median())
-df['Low'] = df['Low'].fillna(value=df['Low'].median())
-df['Close'] = df['Close'].fillna(value=df['Close'].median())
-df['Adj Close'] = df['Adj Close'].fillna(value=df['Adj Close'].median())
-df['Volume'] = df['Volume'].fillna(value=df['Volume'].median())
 
 
-# Konversi kolom 'Date' menjadi tipe datetime
-df['Date'] = pd.to_datetime(df['Open'])
+st.title("UTS PPW KELAS A (MODELLING)")
+st.write("-------------------------------------------------------------------------------------------------------------------------")
+st.write("**Nama  : Ovadilla Aisyah Rahma**")
+st.write("**NIM   : 200411100033**")
+st.write("-------------------------------------------------------------------------------------------------------------------------")
+upload_data, desc, preporcessing, modeling, implementation = st.tabs(["Upload Data","Dataset Description", "Preprocessing", "Modeling", "Implementation"])
 
-# Menampilkan data harga emas
-st.write('Data:', df)
 
-# Memisahkan data menjadi data train dan data test
-data = df['Open']
-n = len(data)
-sizeTrain = (round(n * 0.8))
-data_train = pd.DataFrame(data[:sizeTrain])
-data_test = pd.DataFrame(data[sizeTrain:])
+with upload_data:
+    st.write("""# Upload File""")
+    st.write("Dataset yang digunakan dari PTA Trunojoyo Madura yang sudah dikelola dengan label (Komputasi dan RPL) dan dilakukan pemodelan. Dataset yang dihasilkan yaitu dataset LDA K-Means")
+    uploaded_files = st.file_uploader("Upload file CSV", accept_multiple_files=True)
+    for uploaded_file in uploaded_files:
+        df = pd.read_csv(uploaded_file, sep=';')
+        st.write("Nama File Anda = ", uploaded_file.name)
+        st.dataframe(df)
 
-# Praproses data dengan MinMaxScaler
-scaler = MinMaxScaler()
-train_scaled = scaler.fit_transform(data_train)
-test_scaled = scaler.transform(data_test)
+with desc:
+#    st.write("""# Deskripsi""")
+#    st.write("Dataset ini adalah dataset menengenai penyakit cardiovascular. Kardiovaskular atau Cardiovascular Disease (CVD) merupakan penyakit yang berkaitan dengan jantung dan pembuluh darah. Pada dataset ini terdapat 70.000 data.")
+#    st.write("Dataset ini diambil dari https://www.kaggle.com/code/ekramasif/cardiovasculardiseasepredictionusingml")
+    st.subheader("""Fitur""")
+    st.write("[1].  Age")
+    st.write("[2].  Height")
+    st.write("[3].  Weight")
+    st.write("[4].  Gender")
+    st.write("[5].  Ap_hi (Systolic Blood Pressure)")
+    st.write("[6].  Ap_lo (Diastolic Blood Pressure)")
+    st.write("[7].  Cholesterol")
+    st.write("[8].  Gluc (Glucose)")
+    st.write("[9].  Smoke (Smoking)")
+    st.write("[10]. Alco (Alcohol)")
+    st.write("[11]. Active (Physical Activity)")
+    st.subheader("""Fungsi""")
+    st.write("Aplikasi ini berfungsi untuk menentukan penyakit Cardiovascular.")
+    st.write("Source Code dapat diakses melalui https://github.com/Dhitaaprilia/DataminingApp.git")
 
-# reshaped_data = data.reshape(-1, 1)
-train = pd.DataFrame(train_scaled, columns = ['data'])
-train = train['data']
-# st.write('Data Train',train)
+with preporcessing :
+    st.write("""# Preprocessing""")
+    st.subheader("""Normalisasi Data""")
+    st.write("""Rumus Normalisasi Data :""")
+    from PIL import Image
+    image = Image.open('rumus.jpeg')
+    st.image(image, use_column_width=False, width=250)
+    st.markdown("""
+    Dimana :
+    - X = data yang akan dinormalisasi atau data asli
+    - min = nilai minimum semua data asli
+    - max = nilai maksimum semua data asli
+    """)
+    df = df.drop(columns=["id"])
+    #Mendefinisikan Varible X dan Y
+    X = df.drop(columns=['cardio'])
+    y = df['cardio'].values
+    df
+    X
+    df_min = X.min()
+    df_max = X.max()
+    
+    #NORMALISASI NILAI X
+    scaler = MinMaxScaler()
+    #scaler.fit(features)
+    #scaler.transform(features)
+    scaled = scaler.fit_transform(X)
+    features_names = X.columns.copy()
+    #features_names.remove('label')
+    scaled_features = pd.DataFrame(scaled, columns=features_names)
 
-test = pd.DataFrame(test_scaled, columns = ['data'])
-test = test['data']
+    st.subheader('Hasil Normalisasi Data')
+    st.write(scaled_features)
 
-# Menggabungkan train dan test menjadi satu tabel
-merged_data = pd.concat([train, test], axis=1)
-merged_data.columns = ['Train', 'Test']
-# Menampilkan tabel hasil penggabungan
-st.write('Hasil Normalisasi:')
-st.write(merged_data)
+    st.subheader('Target Label')
+    dumies = pd.get_dummies(df.cardio).columns.values.tolist()
+    dumies = np.array(dumies)
 
-# Membentuk sequence untuk training dan testing
-def split_sequence(sequence, n_steps):
-    X, y = list(), list()
-    for i in range(len(sequence)):
-        end_ix = i + n_steps
-        if end_ix > len(sequence)-1:
-            break
-        seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
-        X.append(seq_x)
-        y.append(seq_y)
-    X = array(X)
-    y = array(y)
-    X = X.reshape((X.shape[0], X.shape[1], 1))  # Perubahan bentuk data
-    return X, y
+    labels = pd.get_dummies(df.cardio).columns.values.tolist()
 
-# Menyiapkan data train
-X_train, Y_train = split_sequence(train_scaled, 2)
-x_train = pd.DataFrame(X_train.reshape((X_train.shape[0], X_train.shape[1])), columns=['xt-2', 'xt-1'])  # Perubahan bentuk data
-y_train = pd.DataFrame(Y_train, columns=['xt'])
-dataset_train = pd.concat([x_train, y_train], axis=1)
-X_train = dataset_train.iloc[:, :2].values
-Y_train = dataset_train.iloc[:, -1].values
+    st.write(labels)
 
-# Menyiapkan data test
-test_x, test_y = split_sequence(test_scaled, 2)
-x_test = pd.DataFrame(test_x.reshape((test_x.shape[0], test_x.shape[1])), columns=['xt-2', 'xt-1'])  # Perubahan bentuk data
-y_test = pd.DataFrame(test_y, columns=['xt'])
-dataset_test = pd.concat([x_test, y_test], axis=1)
-# st.write('Dataset Test:', dataset_test)
-X_test = dataset_test.iloc[:, :2].values
-Y_test = dataset_test.iloc[:, -1].values
+    # st.subheader("""Normalisasi Data""")
+    # st.write("""Rumus Normalisasi Data :""")
+    # st.image('https://i.stack.imgur.com/EuitP.png', use_column_width=False, width=250)
+    # st.markdown("""
+    # Dimana :
+    # - X = data yang akan dinormalisasi atau data asli
+    # - min = nilai minimum semua data asli
+    # - max = nilai maksimum semua data asli
+    # """)
+    # df.weather.value_counts()
+    # df = df.drop(columns=["date"])
+    # #Mendefinisikan Varible X dan Y
+    # X = df.drop(columns=['weather'])
+    # y = df['weather'].values
+    # df_min = X.min()
+    # df_max = X.max()
 
-# Melakukan prediksi dengan Linear Regression
-regressor = LinearRegression()
-regressor.fit(X_train, Y_train)
-knn_preds = regressor.predict(X_test)
+    # #NORMALISASI NILAI X
+    # scaler = MinMaxScaler()
+    # #scaler.fit(features)
+    # #scaler.transform(features)
+    # scaled = scaler.fit_transform(X)
+    # features_names = X.columns.copy()
+    # #features_names.remove('label')
+    # scaled_features = pd.DataFrame(scaled, columns=features_names)
 
-from sklearn.tree import DecisionTreeRegressor
-dt_model = DecisionTreeRegressor()
-dt_model.fit(X_train, Y_train)
-dt_preds = dt_model.predict(X_test)
+    # #Save model normalisasi
+    # from sklearn.utils.validation import joblib
+    # norm = "normalisasi.save"
+    # joblib.dump(scaled_features, norm) 
 
-from sklearn.ensemble import RandomForestRegressor
-rf_model = RandomForestRegressor()
-rf_model.fit(X_train, Y_train)
-rf_preds = rf_model.predict(X_test)
 
-from sklearn.linear_model import LinearRegression
-lr_model = LinearRegression()
-lr_model.fit(X_train, Y_train)
-lr_preds = lr_model.predict(X_test)
+    # st.subheader('Hasil Normalisasi Data')
+    # st.write(scaled_features)
 
-from sklearn.svm import SVR
-svr_model = SVR()
-svr_model.fit(X_train, Y_train)
-svr_preds = svr_model.predict(X_test)
+with modeling:
+    st.write("""# Modeling""")
+    training, test = train_test_split(scaled_features,test_size=0.2, random_state=1)#Nilai X training dan Nilai X testing
+    training_label, test_label = train_test_split(y, test_size=0.2, random_state=1)#Nilai Y training dan Nilai Y testing
+    with st.form("modeling"):
+        st.write("Pilihlah model yang akan dilakukan pengecekkan akurasi:")
+        naive = st.checkbox('Gaussian Naive Bayes')
+        k_nn = st.checkbox('K-Nearest Neighboor')
+        destree = st.checkbox('Decission Tree')
+        submitted = st.form_submit_button("Submit")
 
-# Mengembalikan skala data ke aslinya
-knn_preds = scaler.inverse_transform(knn_preds.reshape(-1, 1))
-dt_preds = scaler.inverse_transform(dt_preds.reshape(-1, 1))
-rf_preds = scaler.inverse_transform(rf_preds.reshape(-1, 1))
-lr_preds = scaler.inverse_transform(lr_preds.reshape(-1, 1))
-svr_preds = scaler.inverse_transform(svr_preds.reshape(-1, 1))
+        # NB
+        GaussianNB(priors=None)
 
-reshaped_datates = Y_test.reshape(-1, 1)
-actual_test = scaler.inverse_transform(reshaped_datates)
+        # Fitting Naive Bayes Classification to the Training set with linear kernel
+        gaussian = GaussianNB()
+        gaussian = gaussian.fit(training, training_label)
 
-# Menyimpan hasil prediksi dan data aktual dalam file Excel
-prediksi_knn = pd.DataFrame(knn_preds)
-prediksi_dt = pd.DataFrame(dt_preds)
-prediksi_rf = pd.DataFrame(rf_preds)
-prediksi_lr = pd.DataFrame(lr_preds)
-prediksi_svr = pd.DataFrame(svr_preds)
+        # Predicting the Test set results
+        y_pred = gaussian.predict(test)
+    
+        y_compare = np.vstack((test_label,y_pred)).T
+        gaussian.predict_proba(test)
+        gaussian_akurasi = round(100 * accuracy_score(test_label, y_pred))
+        # akurasi = 10
 
-actual = pd.DataFrame(actual_test)
+        #Gaussian Naive Bayes
+        # gaussian = GaussianNB()
+        # gaussian = gaussian.fit(training, training_label)
 
-# Menghitung mean absolute percentage error (MAPE)
-knn_mape = mean_absolute_percentage_error(knn_preds, actual_test) * 100
-dt_mape = mean_absolute_percentage_error(dt_preds, actual_test) * 100
-rf_mape = mean_absolute_percentage_error(rf_preds, actual_test)* 100
-lr_mape = mean_absolute_percentage_error(lr_preds, actual_test) * 100
-svr_mape = mean_absolute_percentage_error(svr_preds, actual_test) * 100
+        # probas = gaussian.predict_proba(test)
+        # probas = probas[:,1]
+        # probas = probas.round()
 
-# Menampilkan hasil prediksi dan MAPE
-# st.write("Hasil Prediksi:")
-# st.write(prediksi_knn)
-# st.write("Data Aktual:")
-# st.write(aktual)
-st.write("MAPE KNeighborsRegressor:", knn_mape)
-st.write("MAPE DecisionTreeRegressor:", dt_mape)
-st.write("MAPE RandomForestRegressor:", rf_mape)
-st.write("MAPE LinearRegression:", lr_mape)
-st.write("MAPE SVR:", svr_mape)
+        # gaussian_akurasi = round(100 * accuracy_score(test_label,probas))
 
-# Input tanggal untuk memprediksi harga emas
-st.sidebar.title("Prediksi Harga Saham")
-selected_date = st.sidebar.date_input("Pilih Tanggal")
+        #KNN
+        K=10
+        knn=KNeighborsClassifier(n_neighbors=K)
+        knn.fit(training,training_label)
+        knn_predict=knn.predict(test)
 
-if selected_date is not None:
-    # Ubah tanggal menjadi format yang sesuai dengan data
-    selected_date_str = selected_date.strftime("%Y-%m-%d")
+        knn_akurasi = round(100 * accuracy_score(test_label,knn_predict))
 
-    # Cari indeks tanggal terdekat dalam data
-    closest_date = pd.Timestamp(selected_date)  # Convert to pd.Timestamp
-    closest_date_idx = df['Date'].sub(closest_date).abs().idxmin()
+        #Decission Tree
+        dt = DecisionTreeClassifier()
+        dt.fit(training, training_label)
+        # prediction
+        dt_pred = dt.predict(test)
+        #Accuracy
+        dt_akurasi = round(100 * accuracy_score(test_label,dt_pred))
 
-    # Ambil data sebelum dan pada tanggal yang dipilih
-    selected_data = df.loc[closest_date_idx-2:closest_date_idx, 'Open']
+        if submitted :
+            if naive :
+                st.write('Model Naive Bayes accuracy score: {0:0.2f}'. format(gaussian_akurasi))
+            if k_nn :
+                st.write("Model KNN accuracy score : {0:0.2f}" . format(knn_akurasi))
+            if destree :
+                st.write("Model Decision Tree accuracy score : {0:0.2f}" . format(dt_akurasi))
+        
+        grafik = st.form_submit_button("Grafik akurasi semua model")
+        if grafik:
+            data = pd.DataFrame({
+                'Akurasi' : [gaussian_akurasi, knn_akurasi, dt_akurasi],
+                'Model' : ['Gaussian Naive Bayes', 'K-NN', 'Decission Tree'],
+            })
 
-    # Praproses data dengan MinMaxScaler
-    selected_data_scaled = scaler.transform(selected_data.values.reshape(-1, 1))
+            chart = (
+                alt.Chart(data)
+                .mark_bar()
+                .encode(
+                    alt.X("Akurasi"),
+                    alt.Y("Model"),
+                    alt.Color("Akurasi"),
+                    alt.Tooltip(["Akurasi", "Model"]),
+                )
+                .interactive()
+            )
+            st.altair_chart(chart,use_container_width=True)
+  
+with implementation:
+    st.write("""# Implementasi""")
+    with st.form("my_form"):
+        Age = st.number_input('Usia')
+        Gender = st.number_input('Jenis Kelamin')
+        Height = st.number_input('Tinggi Badan')
+        Weight = st.number_input('Berat Badan')
+        Ap_hi = st.number_input('Systolic Blood Pressure')
+        Ap_lo = st.number_input('Diastolic Blood Pressure')
+        Cholesterol = st.number_input('Cholesterol')
+        Glucose = st.number_input('Glukosa')
+        Smoke = st.number_input('Smoking')
+        Alco = st.number_input('Alcohol')
+        Active = st.number_input('Physical Activity')
+        model = st.selectbox('Pilihlah model yang akan anda gunakan untuk melakukan prediksi?',
+                ('Gaussian Naive Bayes', 'K-NN', 'Decision Tree'))
 
-    # Bentuk input sequence untuk prediksi
-    X_selected, _ = split_sequence(selected_data_scaled, 2)
-    x_selected = pd.DataFrame(X_selected.reshape((X_selected.shape[0], X_selected.shape[1])), columns=['xt-2', 'xt-1'])
-    X_selected = x_selected.values
+        prediksi = st.form_submit_button("Submit")
+        if prediksi:
+            inputs = np.array([
+            Age,Gender,Height,Weight,Ap_hi,Ap_lo,Cholesterol,Glucose,Smoke,Alco,Active
+            ])
 
-    # Prediksi harga emas pada tanggal yang dipilih
-    predicted_price_scaled = regressor.predict(X_selected)
-    predicted_price = scaler.inverse_transform(predicted_price_scaled.reshape(-1, 1))
+            df_min = X.min()
+            df_max = X.max()
+            input_norm = ((inputs - df_min) / (df_max - df_min))
+            input_norm = np.array(input_norm).reshape(1, -1)
 
-    st.sidebar.write("Prediksi Harga Saham pada Tanggal", selected_date_str)
-    st.sidebar.write(predicted_price)
+            if model == 'Gaussian Naive Bayes':
+                mod = gaussian
+            if model == 'K-NN':
+                mod = knn 
+            if model == 'Decision Tree':
+                mod = dt
+
+            input_pred = mod.predict(input_norm)
+
+
+            st.subheader('Hasil Prediksi')
+            st.write('Menggunakan Pemodelan :', model)
+
+            st.write(input_pred)
