18d0de75eff0665f02a5a122c10e272dabfa3dde
OvadillaAisyahRahma <83963385+OvadillaAisyahRahma@users.noreply.github.com>
2023-10-12 09:51:50 +0700

    Update uts.py

--- uts.py
+++ uts.py
@@ -78,8 +78,8 @@ with preporcessing :
 
 with modeling:
     st.write("""# Modeling""")
-    training, test = train_test_split(scaled_features,test_size=0.2, random_state=1)#Nilai X training dan Nilai X testing
-    training_label, test_label = train_test_split(y, test_size=0.2, random_state=1)#Nilai Y training dan Nilai Y testing
+    training, test = train_test_split(scaled_features,test_size=0.2, random_state=42)#Nilai X training dan Nilai X testing
+    training_label, test_label = train_test_split(y, test_size=0.2, random_state=42)#Nilai Y training dan Nilai Y testing
     with st.form("modeling"):
         st.write("Pilihlah model yang akan dilakukan pengecekkan akurasi:")
         naive = st.checkbox('Gaussian Naive Bayes')
